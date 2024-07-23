9655a218564a7ee938eb709ea54ef858fe210893
PK-Trades <155648784+PK-Trades@users.noreply.github.com>
2024-07-23 23:00:58 +0200

    Update working.py

--- working.py
+++ working.py
@@ -41,7 +41,7 @@ def process_data(data, thresholds, older_than_date):
 
 def main():
     st.title("Patrick's Cleanup Tool")
-    st.write("Hier onder kun je aangeven waar je post MINIMAAL aan moet voldoen om NIET in aanmerking te komen voor verwijdering./n Wanneer je een waarde van 1000 invult bij Sessie komen alle URLs met minder dan 1000 sessie in aanmerking voor verwijderen (als alle overige metrics ook kloppen)")
+    st.write("Hier onder kun je aangeven waar je post MINIMAAL aan moet voldoen om NIET in aanmerking te komen voor verwijdering.  /n Wanneer je een waarde van 1000 invult bij Sessie komen alle URLs met minder dan 1000 sessie in aanmerking voor verwijderen (als alle overige metrics ook kloppen)")
     st.markdown("Maak een kopie van het template hieronder en vul deze met jouw data. "
                 "Vervolgens kun je hem hierboven uploaden en zal de tool aan de hand van de door jou ingestelde criteria de URLs die wegkunnen markeren")
     st.markdown("[het template hieronder](https://docs.google.com/spreadsheets/d/1GtaLaXO62Rf8Xo2gNiw6wkAXrHoE-bBJr8Uf3_e8lNw/edit?usp=sharing)")
