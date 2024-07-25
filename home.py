7b58193e1ead85f0a55710278c5a76d3545fddb3
PK-Trades <155648784+PK-Trades@users.noreply.github.com>
2024-07-25 14:04:58 +0200

    Create home.py

---
+++ home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Patrick's Tools",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Your Combined App! 👋")
+
+st.sidebar.success("Select a page above.")
+
+st.markdown(
+    """
+    Selecteer welke tool je wilt gebruiken.
+    """
+)
